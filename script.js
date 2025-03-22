184cef0068cc53210c150b84c15025c383e0d1b3
Shruti Rai <shruti.rai2901@gmail.com>
2025-03-22 19:05:36 +0530

    patient

--- script.js
+++ script.js
@@ -30,26 +30,20 @@ sliderTabs.forEach((tab, index) => {
 updateIndicator(sliderTabs[0], 0);
 window.addEventListener("resize", () => updateIndicator(sliderTabs[swiper.activeIndex], swiper.activeIndex)); 
 
-// Get the modal
 const modal = document.getElementById("adModal");
 
-// Get the button that opens the modal
 const btn = document.getElementById("adBtn");
 
-// Get the <span> element that closes the modal
 const span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal
 btn.onclick = function() {
     modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
